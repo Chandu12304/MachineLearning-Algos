59c1937c26cb29fdc6ae1e677fcea86887af5918
Chandan U <chandanucodeid04@gmail.com>
2025-09-10 19:53:19 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,37 +1,37 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1/(1+np.exp(-x))
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+def sigmoidDerivative(x):
+    return x*(1-x)
 
-X = np.array([[0,0],[0,1],[1,0],[1,1]])
-y = np.array([[0],[1],[1],[0]])
+
+x=np.array([[0,0],[0,1],[1,0],[1,1]])
+y=np.array([[0],[0],[0],[1]])
+
+input_nodes=2
+hidden_nodes=6
+output_nodes=1
 
 np.random.seed(1)
-input_neurons = 2
-hidden_neurons = 2
-output_neurons = 1
+W1=np.random.uniform(-1,1,(input_nodes,hidden_nodes))
+W2=np.random.uniform(-1,1,(hidden_nodes,output_nodes))
 
-W1 = np.random.uniform(-1,1,(input_neurons, hidden_neurons))
-W2 = np.random.uniform(-1,1,(hidden_neurons, output_neurons))
-
-lr = 0.5
+lr=0.1
 for cnt in range(10000):
-    hidden_input = np.dot(X, W1)
-    hidden_output = sigmoid(hidden_input)
+    hidden_in=np.dot(x,W1)
+    hidden_out=sigmoid(hidden_in)
 
-    final_input = np.dot(hidden_output, W2)
-    final_output = sigmoid(final_input)
+    final_in=np.dot(hidden_out,W2)
+    final_out=sigmoid(final_in)
 
-    error = y - final_output
+    error=y-final_out
 
-    d_output = error * sigmoid_derivative(final_output)
-    d_hidden = d_output.dot(W2.T) * sigmoid_derivative(hidden_output)
+    d_out=error*sigmoidDerivative(final_out)
+    d_hidden=d_out.dot(W2.T)*sigmoidDerivative(hidden_out)
 
-    W2 += hidden_output.T.dot(d_output) * lr
-    W1 += X.T.dot(d_hidden) * lr
+    W2+=hidden_out.T.dot(d_out)*lr
+    W1+=x.T.dot(d_hidden)*lr
 
-print("Predicted Output after training:")
-print(final_output)
+print("final o/p: ", np.round(final_out,3))
