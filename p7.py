f80a939c95110cfc1615833b61148108f808f69e
Chandan U <chandanucodeid04@gmail.com>
2025-09-10 13:56:41 +0530

    Update p7.py

--- p7.py
+++ p7.py
@@ -2,39 +2,23 @@ import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-# Step 1: Define the training data as a list of lists
-data = [
-    [1, 2],
-    [1, 4],
-    [1, 0],
-    [10, 2],
-    [10, 4],
-    [10, 0]
-]
+data=[[2.74, 3.58], [3.92, 0.93], [2.92, 2.19], [2.71, 4.01],
+ [1.36, 1.27], [4.37, 2.9], [0.71, 4.46], [4.19, 1.18],
+ [2.98, 4.49], [2.23, 3.88]]
 
-# Convert to DataFrame
-df = pd.DataFrame(data, columns=["X", "Y"])
-print("Data:")
+df=pd.DataFrame(data,columns=["X","Y"])
 print(df)
 
-# Step 2: Initialize K-Means (2 clusters)
-kmeans = KMeans(n_clusters=4, random_state=0)
-
-# Step 3: Train the model
+kmeans=KMeans(n_clusters=2,random_state=0)
 kmeans.fit(df)
 
-# Step 4: Get cluster labels
-labels = kmeans.labels_
-print("\nCluster labels for each data point:")
+labels=kmeans.labels_
 print(labels)
 
-# Step 5: Get cluster centers
-centers = kmeans.cluster_centers_
-print("\nCluster centers:")
+centers=kmeans.cluster_centers_
 print(centers)
 
-# Step 6: Graphical Representation
-plt.scatter(df.X, df.Y, c=labels)      # points
-plt.scatter(*centers.T, c="red", s=200, marker="X")  # centers
+plt.scatter(df.X,df.Y,c=labels)
+plt.scatter(*centers.T,c="red",s=100,marker="X")
+plt.grid()
 plt.show()
-
