ee18ddf63db475bb28dc512eab8216a81d400ec6
Chandan U <chandanucodeid04@gmail.com>
2025-09-10 16:55:38 +0530

    Update p8.py

--- p8.py
+++ p8.py
@@ -1,26 +1,36 @@
+#KNearestNeighbors
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 
 data = [
-    [170, 70, "Male"],
-    [165, 55, "Female"],
-    [180, 80, "Male"],
-    [175, 75, "Male"],
-    [160, 50, "Female"],
-    [155, 45, "Female"],
-    [185, 90, "Male"],
-    [170, 65, "Female"]
+    [170, 70, "male"],
+    [165, 55, "female"],
+    [180, 80, "male"],
+    [175, 75, "male"],
+    [160, 50, "female"],
+    [155, 45, "female"],
+    [185, 90, "male"],
+    [170, 65, "female"]
 ]
-df = pd.DataFrame(data, columns=["Height", "Weight", "Gender"])
 
-X = df[["Height", "Weight"]]
-y = df["Gender"]
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X, y)
+df=pd.DataFrame(data,columns=["height","weight","gender"])
+x=df[["height","weight"]]
+y=df["gender"]
 
-h = int(input("Enter Height: "))
-w = int(input("Enter Weight: "))
+knn=KNeighborsClassifier(n_neighbors=3)
+knn.fit(x,y)
 
-new_data=pd.DataFrame([[h,w]],columns=["Heaight","Weight"])
+h=int(input("height: "))
+w=int(input("weight: "))
 
-print("Predicted Gender:", knn.predict(new_data)[0])
+new_data=pd.DataFrame([[h,w]],columns=["height","weight"])
+
+pred=knn.predict(new_data)[0]
+print(pred)
+
+colors={"male":"blue","female":"red"}
+plt.scatter(x["height"], x["weight"], c=y.map(colors))
+plt.scatter(h, w, c="green", s=100, marker='*') 
+plt.grid()
+plt.show()
